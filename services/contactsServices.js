5285659fd2ee4fd014ddfe62bfd71578971e1a35
Yevhen <benatanlev@icloud.com>
2025-06-13 13:34:54 +0300

    +

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -7,21 +7,21 @@ const contactsPath = path.resolve("db", "contacts.json");
 const updateListContact = (contacts) =>
   fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-export const listContacts = async () => {
+export const getAllContacts = async () => {
   const data = await fs.readFile(contactsPath, "utf-8");
 
   return JSON.parse(data);
 };
 
-export const getContactById = async (contactId) => {
-  const contacts = await listContacts();
+export const getOneContact = async (contactId) => {
+  const contacts = await getAllContacts();
   const contact = contacts.find((item) => item.id === contactId);
 
   return contact || null;
 };
 
-export const addContact = async (data) => {
-  const contacts = await listContacts();
+export const createContact = async (data) => {
+  const contacts = await getAllContacts();
   const newContact = {
     id: nanoid(),
     ...data,
@@ -32,12 +32,23 @@ export const addContact = async (data) => {
   return newContact;
 };
 
-export const removeContact = async (contactId) => {
-  const contacts = await listContacts();
+export const deleteContact = async (contactId) => {
+  const contacts = await getAllContacts();
   const idx = contacts.findIndex((item) => item.id === contactId);
   if (idx === -1) return null;
   const [result] = contacts.splice(idx, 1);
   await updateListContact(contacts);
 
   return result;
+};
+
+export const updateContact = async (contactId, newData) => {
+  const contacts = await getAllContacts();
+  const idx = contacts.findIndex((item) => item.id === contactId);
+  if (idx === -1) return null;
+
+  contacts[idx] = { ...contacts[idx], ...newData };
+  await updateListContact(contacts);
+
+  return contacts[idx];
 };
