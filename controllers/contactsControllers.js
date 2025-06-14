362f23834e7457c17bdbd8fbaf23246205e79900
Yevhen <benatanlev@icloud.com>
2025-06-14 11:39:17 +0300

    fix - put push and message-not found

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -13,24 +13,32 @@ const getOneContactController = async (req, res) => {
   const result = await contactsService.getOneContact(id);
 
   if (!result) {
-    throw HttpError(404, `Contact with id: ${id} not found`);
+    throw HttpError(404);
   }
 
   res.json(result);
 };
 
 const createContactController = async (req, res) => {
+  if (!Object.keys(req.body).length) {
+    throw HttpError(400, "Body must have at least one field");
+  }
+
   const result = await contactsService.createContact(req.body);
 
   res.status(201).json(result);
 };
 
 const updateContactController = async (req, res) => {
+  if (!Object.keys(req.body).length) {
+    throw HttpError(400, "Body must have at least one field");
+  }
+
   const { id } = req.params;
   const result = await contactsService.updateContact(id, req.body);
 
   if (!result) {
-    throw HttpError(404, `Contact with id: ${id} not found`);
+    throw HttpError(404);
   }
 
   res.json(result);
@@ -41,7 +49,7 @@ const deleteContactController = async (req, res) => {
   const result = await contactsService.deleteContact(id);
 
   if (!result) {
-    throw HttpError(404, `Contact with id: ${id} not found`);
+    throw HttpError(404);
   }
 
   res.json(result);
